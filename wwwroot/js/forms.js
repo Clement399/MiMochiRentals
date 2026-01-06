6910fa8f326d6a403642939e231a20d198455050
Clement <clementwongjj@gmail.com>
2026-01-06 15:15:04 +0800

    Fixed form.js logic to handle multiple products in itemTypeCode, polymorphism

--- wwwroot/js/forms.js
+++ wwwroot/js/forms.js
@@ -5,7 +5,7 @@ allForms.forEach(f => {
     f.addEventListener('submit', (e) => submitRental(e))
 });
 console.log("Hello im the forms.js javascript file for forms")
-async function submitRental(e) {
+function submitRental(e) {
     //prevents page reloading
     e.preventDefault();
 
@@ -14,34 +14,66 @@ async function submitRental(e) {
 
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData.entries());
-    let tempItem = itemTypesMap[data.itemTypeCode];
-    console.error("Temp item : ", tempItem);
-    let itemTypeCode = data.itemTypeCode;
-    let price = tempItem.price;
-    let bond = tempItem.bond;
-    let name = tempItem.name;
-    console.log('Form Data : ', data);
+    console.log("data from form :", data);
+
     const terms = document.getElementById('myCheckBox');
     console.error("Checked? : ", terms.checked);
     console.log(document.getElementById(myCheckBox));
     if (!terms.checked) {
         alert('Ensure terms and conditions are accepted');
         //Document.getElementById('termsErrorBox').classlist.remove('hidden');
-        return;
     }
-    const rentalItem = {
-        itemTypeCode,
-        quantity: parseInt(data.quantity),
-        startDate: data.startDate,
-        startTime: data.startTime,
-        endDate: data.endDate,
-        endTime: data.endTime,
-        price,
-        bond,
-        name
-    };
-    console.log("Product : ", rentalItem);
-    await processRental(rentalItem);
+    else {
+        if (data.itemTypeCode.length > 15) {
+            const itemTypeArray = JSON.parse(data.itemTypeCode)
+            console.log("itemTypeCode is an Array", itemTypeArray);
+            itemTypeArray.forEach(i => {
+                console.log("item ", i)
+                let tempItem = itemTypesMap[i];
+                console.error("Temp item : ", tempItem);
+                let itemTypeCode = data.itemTypeCode;
+                let price = tempItem.price;
+                let bond = tempItem.bond;
+                let name = tempItem.name;
+                const rentalItem = {
+                    itemTypeCode : i,
+                    quantity: parseInt(data.quantity),
+                    startDate: data.startDate,
+                    startTime: data.startTime,
+                    endDate: data.endDate,
+                    endTime: data.endTime,
+                    price,
+                    bond,
+                    name
+                };
+                console.log("Product : ", rentalItem);
+                processRental(rentalItem);
+            });
+        }
+        else {
+            console.log("Not an array");
+            let tempItem = itemTypesMap[data.itemTypeCode];
+            console.error("Temp item : ", tempItem);
+            let itemTypeCode = data.itemTypeCode;
+            let price = tempItem.price;
+            let bond = tempItem.bond;
+            let name = tempItem.name;
+            console.log('Form Data : ', data);
+            const rentalItem = {
+                itemTypeCode,
+                quantity: parseInt(data.quantity),
+                startDate: data.startDate,
+                startTime: data.startTime,
+                endDate: data.endDate,
+                endTime: data.endTime,
+                price,
+                bond,
+                name
+            };
+            console.log("Product : ", rentalItem);
+            processRental(rentalItem);
+        }
+    }
 }
 
 async function processRental(rentalItem){
